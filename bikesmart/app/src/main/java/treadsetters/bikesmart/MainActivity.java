2c06dd3a301be2c62811b4633a8f4fe0f7561eb8
Oliver Townsend <otownsend92@gmail.com>
2015-01-22 15:22:24 -0800

    Added basic Parse config. Let me know if you have issues getting Parse setup. You might have to go and download the quickstart package from the Parse website

--- bikesmart/app/src/main/java/treadsetters/bikesmart/MainActivity.java
+++ bikesmart/app/src/main/java/treadsetters/bikesmart/MainActivity.java
@@ -4,14 +4,25 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.parse.Parse;
+
+
 
 
 public class MainActivity extends ActionBarActivity {
+
+    public static String key1 = "Iy2F0D1cUhwVVeVBt9Akg6ovargei7nIZqUItdr0";
+    public static String key2 = "8rUgARpUSv8v1N7a7sN1DeLlnact6mITLLC4Bty5";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this, key1, key2);
+
     }
 
 
