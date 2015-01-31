d68ace38bfbea13db95645676e4f569fc7169bcb
Oliver Townsend <otownsend92@gmail.com>
2015-01-31 14:54:36 -0800

    location button fix. wrong activity layout.

--- bikesmart/app/src/main/java/treadsetters/bikesmart/MainActivity.java
+++ bikesmart/app/src/main/java/treadsetters/bikesmart/MainActivity.java
@@ -53,7 +53,7 @@ public class MainActivity extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
+        setContentView(R.layout.activity_main);
 
         mLatitudeText = (TextView) findViewById((R.id.latitude_text));
         mLongitudeText = (TextView) findViewById((R.id.longitude_text));
