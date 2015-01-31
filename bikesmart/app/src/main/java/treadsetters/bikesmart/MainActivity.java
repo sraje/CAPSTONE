33140c5d6ad7d6714c5ecb8069ce5b907a2c0f16
Chris Karcher <chrisk@procore.com>
2015-01-30 21:48:10 -0800

    network connectivity manager

--- bikesmart/app/src/main/java/treadsetters/bikesmart/MainActivity.java
+++ bikesmart/app/src/main/java/treadsetters/bikesmart/MainActivity.java
@@ -1,9 +1,11 @@
 package treadsetters.bikesmart;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+
 import com.parse.Parse;
 
 
@@ -14,6 +16,8 @@ public class MainActivity extends ActionBarActivity {
     public static String key1 = "Iy2F0D1cUhwVVeVBt9Akg6ovargei7nIZqUItdr0";
     public static String key2 = "8rUgARpUSv8v1N7a7sN1DeLlnact6mITLLC4Bty5";
 
+    private boolean isConnected;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +27,11 @@ public class MainActivity extends ActionBarActivity {
         Parse.enableLocalDatastore(this);
         Parse.initialize(this, key1, key2);
 
+        Context context = getApplicationContext();
+        NetworkStuff networkStuff = new NetworkStuff(context);
+        isConnected = networkStuff.getConnectionStatus();
+        // For testing purposes
+        //Log.d("isConnected?", isConnected);
     }
 
 
@@ -38,8 +47,8 @@ public class MainActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
+        int id = item.getItemId();
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
