4c59e133740db8ac18bb0a7035d2b31a5065bc08
sraje <saili_raje@yahoo.com>
2015-03-06 13:05:32 -0800

    ditto
    ditto

--- bikesmart/app/src/main/java/treadsetters/bikesmart/MainActivity2.java
+++ bikesmart/app/src/main/java/treadsetters/bikesmart/MainActivity2.java
@@ -20,17 +20,62 @@ import com.parse.ParseUser;
 /**
  * Activity which displays a login screen to the user, offering registration as well.
  */
-public class MainActivity2 extends Activity {
-    // UI references.
-    private EditText usernameEditText;
-    private EditText passwordEditText;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
+//import android.support.v7.widget.LinearLayoutManager;
+//import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
+
+
+public class MainActivity2 extends ActionBarActivity { /* When using Appcombat support library
+                                                         you need to extend Main Activity to
+                                                         ActionBarActivity.
+                                                      */
+
+
+    private Toolbar toolbar;                              // Declaring the Toolbar Object
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main_activity2);
 
+        toolbar = (Toolbar) findViewById(R.id.tool_bar); // Attaching the layout to the toolbar object
+        setSupportActionBar(toolbar);                   // Setting toolbar as the ActionBar with setSupportActionBar() call
 
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
+
+
