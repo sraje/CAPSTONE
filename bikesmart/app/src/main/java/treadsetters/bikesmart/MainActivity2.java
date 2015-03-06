f37c0e12e7277e0c765b6eafa00afcb580fc92b9
sraje <saili_raje@yahoo.com>
2015-03-06 01:08:11 -0800

    Found the issue
    haven't found a solution yet.

--- bikesmart/app/src/main/java/treadsetters/bikesmart/MainActivity2.java
+++ bikesmart/app/src/main/java/treadsetters/bikesmart/MainActivity2.java
@@ -1,39 +1,36 @@
 package treadsetters.bikesmart;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
-public class MainActivity2 extends ActionBarActivity {
+/**
+ * Activity which displays a login screen to the user, offering registration as well.
+ */
+public class MainActivity2 extends Activity {
+    // UI references.
+    private EditText usernameEditText;
+    private EditText passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main_activity2);
-    }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main_activity2, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
