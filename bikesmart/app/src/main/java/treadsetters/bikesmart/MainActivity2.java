46076b619cb28b6a852a01cf7b85549cd5d5c6b5
sraje <saili_raje@yahoo.com>
2015-03-06 15:31:09 -0800

    navigation fragment keeps crashing the app
    whyyy

--- bikesmart/app/src/main/java/treadsetters/bikesmart/MainActivity2.java
+++ bikesmart/app/src/main/java/treadsetters/bikesmart/MainActivity2.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
+import android.support.v7.app.ActionBar;
 import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,28 +21,43 @@ import com.parse.ParseUser;
 /**
  * Activity which displays a login screen to the user, offering registration as well.
  */
-
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarDrawerToggle;
-//import android.support.v7.widget.LinearLayoutManager;
-//import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Toast;
 
 
-public class MainActivity2 extends ActionBarActivity { /* When using Appcombat support library
-                                                         you need to extend Main Activity to
-                                                         ActionBarActivity.
-                                                      */
+public class MainActivity2 extends ActionBarActivity {
 
+    //First We Declare Titles And Icons For Our Navigation Drawer List View
+
+
+    String TITLES[] = {"Home","Bikes","Friends","Messages","Notifications"};
+    int ICONS[] = {R.drawable.ic_home,R.drawable.ic_bikes, R.drawable.ic_friends,R.drawable.ic_messages,R.drawable.ic_notifications};
+
+    //Similarly we Create a String Resource for the name and email in the header view
+    //And we also create a int resource for profile picture in the header view
+
+    String NAME = "Saili Raje";
+    String EMAIL = "Capstoners2015.com";
+    int PROFILE = R.drawable.aka;
 
     private Toolbar toolbar;                              // Declaring the Toolbar Object
+
+    RecyclerView mRecyclerView;                           // Declaring RecyclerView
+    RecyclerView.Adapter mAdapter;                        // Declaring Adapter For Recycler View
+    RecyclerView.LayoutManager mLayoutManager;            // Declaring Layout Manager as a linear layout manager
+    DrawerLayout Drawer;                                  // Declaring DrawerLayout
+
+    ActionBarDrawerToggle mDrawerToggle;                  // Declaring Action Bar Drawer Toggle
+
+
 
 
     @Override
@@ -49,8 +65,50 @@ public class MainActivity2 extends ActionBarActivity { /* When using Appcombat s
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_activity2);
 
-        toolbar = (Toolbar) findViewById(R.id.tool_bar); // Attaching the layout to the toolbar object
-        setSupportActionBar(toolbar);                   // Setting toolbar as the ActionBar with setSupportActionBar() call
+    /* Assinging the toolbar object ot the view
+    and setting the the Action bar to our toolbar
+     */
+        /*toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+
+
+
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.RecyclerView); // Assigning the RecyclerView Object to the xml View
+
+        mRecyclerView.setHasFixedSize(true);                            // Letting the system know that the list objects are of fixed size
+
+        mAdapter = new MyAdapter(TITLES,ICONS,NAME,EMAIL,PROFILE);       // Creating the Adapter of MyAdapter class(which we are going to see in a bit)
+        // And passing the titles,icons,header view name, header view email,
+        // and header view profile picture
+
+        mRecyclerView.setAdapter(mAdapter);                              // Setting the adapter to RecyclerView
+
+        mLayoutManager = new LinearLayoutManager(this);                 // Creating a layout Manager
+
+        mRecyclerView.setLayoutManager(mLayoutManager);                 // Setting the layout Manager
+
+
+        Drawer = (DrawerLayout) findViewById(R.id.DrawerLayout);        // Drawer object Assigned to the view
+           mDrawerToggle = new ActionBarDrawerToggle(this,Drawer,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close){
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                // code here will execute once the drawer is opened( currently nothing)
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                // Code here will execute once drawer is closed
+            }
+
+
+
+        }; // Drawer Toggle Object Made
+        Drawer.setDrawerListener(mDrawerToggle); // Drawer Listener set to the Drawer toggle
+        mDrawerToggle.syncState();               // Finally we set the drawer toggle sync State
 
     }
 
@@ -75,7 +133,6 @@ public class MainActivity2 extends ActionBarActivity { /* When using Appcombat s
         }
 
         return super.onOptionsItemSelected(item);
+    */
     }
 }
-
-
