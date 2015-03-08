54dfe696115f3e3382d34788a2dde9f6ac5f1de4
sraje <saili_raje@yahoo.com>
2015-03-08 01:21:14 -0800

    is rendering the correct menu now
    and nothing breaks~

--- bikesmart/app/src/main/java/treadsetters/bikesmart/MainActivity2.java
+++ bikesmart/app/src/main/java/treadsetters/bikesmart/MainActivity2.java
@@ -211,7 +211,7 @@ public class MainActivity2 extends ActionBarActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_main_activity2, menu);
         return true;
     }
 
