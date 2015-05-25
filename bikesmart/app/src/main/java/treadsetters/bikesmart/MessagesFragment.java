a82583c9c7bd9f7f0caa274594c573bc6fe4b815
sraje <saili_raje@yahoo.com>
2015-05-24 17:37:26 -0700

    buf fix attempt
    fix only applied to messages fragment for testing purposes

--- bikesmart/app/src/main/java/treadsetters/bikesmart/MessagesFragment.java
+++ bikesmart/app/src/main/java/treadsetters/bikesmart/MessagesFragment.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -64,7 +65,17 @@ public class MessagesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_messages, container, false);
+        final View rootView = inflater.inflate(R.layout.fragment_messages, container, false);
+          /*here is an implementation*/
+
+       rootView.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent event) {
+                return true;
+            }
+        });
+
+
+        return rootView;//inflater.inflate(R.layout.fragment_messages, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
